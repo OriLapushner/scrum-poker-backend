d819f87855445bcbb0accbcedfe891d8accd4ea5
OriLapushner <ori.lapushner@gmail.com>
2025-06-24 19:22:54 -0500

    removed unused function

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,1 @@
 export const uniqueSid = () => Math.random().toString(16).slice(3)
-export const average = (array: number[]): number => Number.parseFloat((array.reduce((a, b) => a + b, 0) / array.length).toFixed(2))
